66e578705237d144a47e58862d249ffcf3cba445
jeongmi <47415255+parkjeongmi@users.noreply.github.com>
2021-07-09 02:27:19 +0900

    hash

--- 0710test/hash_2.cpp
+++ 0710test/hash_2.cpp
@@ -1,11 +1,22 @@
+//해시-완주하지 못한 선수
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
-
-string solution(vector<string> participant, vector<string> completion){
+string solution1(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    std::unordered_map<string, int>d;
+    for (auto&i : participant) d[i]++;
+    for (auto&i : completion) d[i]--;
+    for (auto&i : d){
+        if (i.second!=0) answer = i.first;
+    }
+    return answer;
+}
+string solution2(vector<string> participant, vector<string> completion){
     sort(participant.begin(), participant.end());
     sort(completion.begin(),completion.end());
 
@@ -19,6 +30,6 @@ string solution(vector<string> participant, vector<string> completion){
 int main(){
     vector<string> a = {"leo", "kiki", "eden"};
     vector<string> b = {"eden", "kiki"};
-    cout << solution(a,b) << endl;
+    cout << solution1(a,b) << endl;
     return 0;
 }
--- 0710test/hash_5.cpp
+++ 0710test/hash_5.cpp
@@ -1,10 +1,25 @@
+//해시 : 위장
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <string>
 using namespace std;
 
-int solution(vector<vector<string>> clothes) {
+//unordered_map 활용한 풀이
+int solution1(vector<vector<string>>clothes){
+    int answer = 1;
+    unordered_map <string, int> m;
+    for (auto clo : clothes){
+        m[clo[1]] += 1;
+    }
+    for (auto n : m ){
+        answer *= (n.second+1);
+    }
+    return answer-1;
+}
+
+//map 활용한 풀이
+int solution2(vector<vector<string>> clothes) {
     int answer = 1;
     map<string, int> m;
     for(auto cloth : clothes){
